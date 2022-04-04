90750c7ca34ebb06dbd4ef423646e45cfd64e67d
Kev Morel <kev.morel@northcoders.com>
2022-04-04 21:53:05 +0100

    add Overworld class

---
+++ Overworld.js
@@ -0,0 +1,15 @@
+class Overworld {
+    constructor(config) {
+        this.element = config.element;
+        this.canvas = this.element.querySelector(".game-canvas");
+        this.ctx = this.canvas.getContext("2d")
+    }
+
+init() {
+    const image = new Image();
+    image.onload = () => {
+        
+    };
+}
+
+}
