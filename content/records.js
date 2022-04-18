96aeef9866aedf3864ab9ba6d1cf414e986fb9ff
Kev Morel <kev.morel@northcoders.com>
2022-04-18 11:22:56 +0100

    add content dir

---
+++ content/records.js
@@ -0,0 +1,31 @@
+window.RecordTypes = {
+  normal: 'normal',
+  techno: 'techno',
+  hiphop: 'hiphop',
+  rock: 'rock',
+  blues: 'blues',
+};
+
+window.Records = {
+  t001: {
+    name: 'There can be only one',
+    artist: 'CJ Bolland',
+    type: RecordTypes.techno,
+    src: '/assets/objects/technoRecord.png',
+    icon: '/assets/icons/techno.png',
+  },
+  h001: {
+    name: "Can't stop won't stop",
+    artist: 'KRS One',
+    type: RecordTypes.hiphop,
+    src: '/assets/objects/hiphopRecord.png',
+    icon: '/assets/icons/hiphop.png',
+  },
+  r001: {
+    name: 'Ramble on',
+    artist: 'Led Zepplin',
+    type: RecordTypes.rock,
+    src: '/assets/objects/rockRecord.png',
+    icon: '/assets/icons/rock.png',
+  },
+};
