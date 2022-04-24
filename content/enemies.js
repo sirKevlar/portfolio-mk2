901bbb138e3709845b1f555ebc85786c4cc4783d
Kev Morel <kev.morel@northcoders.com>
2022-04-24 15:42:56 +0100

    add enemies content file

---
+++ content/enemies.js
@@ -0,0 +1,50 @@
+window.Enemies = {
+  mum: {
+    name: "Kev's Mum",
+    src: '/assets/characters/mum.png',
+    fightMsg: 'Kev\'s mum says "Ooh, I hope this doesn\'t hurt you too much"',
+    records: {
+      a: {
+        recordId: 'n001',
+        maxHp: 50,
+        level: 1,
+      },
+      b: {
+        recordId: 'n002',
+        maxHp: 50,
+        level: 1,
+      },
+    },
+  },
+  stan: {
+    name: 'Stan',
+    src: '/assets/characters/skeleton.png',
+    fightMsg: 'Stan says "Luke, I\'m your father" and pulls out a switchblade',
+    records: {
+      a: {
+        recordId: 'r001',
+        maxHp: 50,
+        level: 1,
+      },
+      b: {
+        recordId: 'r002',
+        maxHp: 50,
+        level: 1,
+      },
+    },
+  },
+  steve: {
+    name: 'Steve',
+    src: '/assets/characters/slime.png',
+    fightMsg:
+      'Steve says "I\'m a pacifist" as he rolls up his sleeves and prepares to fight.',
+    records: {
+      a: {
+        hp: 1,
+        recordId: 'n006',
+        maxHp: 50,
+        level: 1,
+      },
+    },
+  },
+};
