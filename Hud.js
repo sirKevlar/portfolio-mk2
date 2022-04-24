fd78b0af3c8245e5d506f5822aa6a640c6e7c22b
Kev Morel <kev.morel@northcoders.com>
2022-04-24 15:39:04 +0100

    add hud.js and styles

---
+++ Hud.js
@@ -0,0 +1,52 @@
+class Hud {
+  constructor() {
+    this.scoreboards = [];
+  }
+
+  update() {
+    this.scoreboards.forEach((s) => {
+      s.update(window.playerState.records[s.id]);
+    });
+  }
+
+  createElement() {
+    if (this.element) {
+      this.element.remove();
+      this.scoreboards = [];
+    }
+
+    this.element = document.createElement('div');
+    this.element.classList.add('Hud');
+
+    const { playerState } = window;
+    playerState.lineup.forEach((key) => {
+      const record = playerState.records[key];
+      const scoreboard = new Combatant(
+        {
+          id: key,
+          ...Records[record.recordId],
+          ...record,
+        },
+        null
+      );
+      scoreboard.createElement();
+      this.scoreboards.push(scoreboard);
+      this.element.appendChild(scoreboard.hudElement);
+    });
+    this.update();
+  }
+
+  init(container) {
+    this.createElement();
+    container.appendChild(this.element);
+
+    document.addEventListener('PlayerStateUpdated', () => {
+      this.update();
+    });
+
+    document.addEventListener('LineupChanged', () => {
+      this.createElement();
+      container.appendChild(this.element);
+    });
+  }
+}
