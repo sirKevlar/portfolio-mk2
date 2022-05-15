f5fd5ed488d4f3e7d0017c90b2a836dd5847de4e
Kev Morel <kev.morel@northcoders.com>
2022-05-15 12:32:33 +0100

    add boombox.js

---
+++ BoomBox.js
@@ -0,0 +1,44 @@
+class BoomBox extends GameObject {
+  constructor(config) {
+    super(config);
+    this.sprite = new Sprite({
+      gameObject: this,
+      src: config.src,
+      animations: {
+        still: [[0, 0]],
+      },
+      currentAnimation: 'still',
+    });
+    this.clickAction = [
+      {
+        events: [
+          {
+            type: 'displayMp3s',
+            mp3s: config.mp3s,
+            descriptions: config.descriptions,
+            cover: config.cover,
+            coverDescription: config.coverDescription,
+          },
+          {
+            type: 'displayImageAndText',
+            src: config.srcs[0],
+            description: config.imageDescriptions[0],
+            text: config.texts[0],
+          },
+          {
+            type: 'displayImageAndText',
+            src: config.srcs[1],
+            description: config.imageDescriptions[1],
+            text: config.texts[1],
+          },
+          {
+            type: 'displayImageAndText',
+            src: config.srcs[2],
+            description: config.imageDescriptions[2],
+            text: config.texts[2],
+          },
+        ],
+      },
+    ];
+  }
+}
